86644289675ff8bfc143fab6c9899a0df9b0ab44
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-07-30 14:29:33 +0500

    test with utility classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-500 h-screen grid grid-cols-3 grid-rows-2 gap-3 p-3">
+      <div className="bg-slate-900"></div>
+      <div className="bg-slate-900"></div>
+      <div className="bg-slate-900"></div>
+      <div className="bg-slate-900"></div>
+      <div className="bg-slate-900"></div>
+      <div className="bg-slate-900"></div>
     </div>
   );
 }
